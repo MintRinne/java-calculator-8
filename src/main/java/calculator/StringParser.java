88b651ea40a829c2d75d5674e77c437e779cf333
박하린 <mintrin@baghalin-ui-MacBookAir.local>
2025-10-20 17:19:00 +0900

    커스텀 구분자 기능 추가

--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -10,6 +10,17 @@ public class StringParser {
 
     public static String[] split(String strInput){
 
+    //커스텀 구분자 패턴 추가
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher((strInput));
+
+    //커스텀 구분자가 있는 경우
+            if(matcher.matches()){
+                String strCustom = matcher.group(1);
+                String strNum = matcher.group(2);
+
+                return strNum.split(strCustom);
+            }
+
         //기본 기분자
         return strInput.split(strDelimiters);
     }
