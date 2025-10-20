e35e4e4e0f467ae0e8d82b96f8279a54dabeb8a2
박하린 <mintrin@baghalin-ui-MacBookAir.local>
2025-10-20 17:13:29 +0900

    기본 구분자 기준으로 분리한 숫자의 합 반환

--- src/main/java/calculator/StringCaculator.java
+++ src/main/java/calculator/StringCaculator.java
@@ -1,12 +1,27 @@
 package calculator;
 
 public class StringCaculator {
+
+    private static int toPositiveInt(String number){
+            int value = Integer.parseInt(number);
+        return value;
+    }
     public static int add(String strInput) {
         //빈문자열 || null 입력시 0 반환
         if (isBlank(strInput)) {
             return 0;
         }
-        return 0;
+
+        //입력 문자열을 구분자 기준으로 분리
+        String[] numbers = StringParser.split(strInput);
+
+        int sum = 0;
+        for(String number : numbers) {
+            //문자열을 숫자로 변환하고, 유효성 검사
+            int pared = toPositiveInt(number);
+            sum += pared;
+        }
+        return sum;
     }
 
     //null || 빈 문자열 확인
--- src/main/java/calculator/StringParser.java
+++ src/main/java/calculator/StringParser.java
@@ -1,4 +1,17 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringParser {
+
+    //기본 구분자 쉼표, 콜론
+    public static final String strDelimiters = ",|:";
+
+    public static String[] split(String strInput){
+
+        //기본 기분자
+        return strInput.split(strDelimiters);
+    }
+
 }
